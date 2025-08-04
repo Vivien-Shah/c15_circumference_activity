edddeca1119adcaa788bf03bd6cf58003dafa4de
Vivien-Shah <hfsthane7787@gmail.com>
2025-08-04 19:08:12 +0530

    Add files via upload

---
+++ c15_circum.py
@@ -0,0 +1,6 @@
+#Write a program to find the circumference of a circle
+def circumference():
+    radius=int(input("Please enter the radius of a circle: "))
+    circumference=2 * 3.14 * radius
+    print("The circumference of a circle is: ",circumference)
+circumference()
